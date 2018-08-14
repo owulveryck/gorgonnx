c4941c877b97fdc7e29c91c07a4f389e33c66fa0
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-08-14 16:07:52 +0200

    Ok

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 
 	"github.com/onnx/onnx"
 	"gonum.org/v1/gonum/graph/encoding/dot"
@@ -21,7 +22,7 @@ func main() {
 		log.Fatal(err)
 	}
 	graph := model.GetGraph()
-	fmt.Println("Graph Name: ", *graph.Name)
+	//fmt.Println("Graph Name: ", *graph.Name)
 
 	// dict maintains a databse of the nodes
 	dict := make(map[string]node, len(graph.Node)+len(graph.Input)+len(graph.Output))
@@ -29,9 +30,11 @@ func main() {
 	g := simple.NewDirectedGraph()
 	id := 0
 	for _, n := range graph.Node {
+		fmt.Println(n)
 		id++
 		dict[*n.Name] = node{
 			int64(id),
+			"",
 			n,
 			nil,
 		}
@@ -42,6 +45,7 @@ func main() {
 		id++
 		dict[*n.Name] = node{
 			int64(id),
+			"",
 			nil,
 			n,
 		}
@@ -52,6 +56,7 @@ func main() {
 		id++
 		dict[*n.Name] = node{
 			int64(id),
+			"",
 			nil,
 			n,
 		}
@@ -59,9 +64,20 @@ func main() {
 	}
 	for _, n := range g.Nodes() {
 		n := n.(node)
+		if n.ID() == 0 {
+			fmt.Println("DEBUG", n)
+		}
 		switch {
 		case n.NodeProto != nil:
 			for _, i := range n.NodeProto.Input {
+				if dict[i].ID() == 0 {
+					// If the node does not exist, add it (it may be an input or output)
+					id := g.NewNode().ID()
+					dict[i] = node{
+						id:   int64(id),
+						name: i,
+					}
+				}
 				e := simple.Edge{
 					F: dict[i],
 					T: n,
@@ -69,13 +85,20 @@ func main() {
 				g.SetEdge(e)
 			}
 			for _, i := range n.NodeProto.Output {
+				if dict[i].ID() == 0 {
+					// If the node does not exist, add it (it may be an input or output)
+					id := g.NewNode().ID()
+					dict[i] = node{
+						id:   int64(id),
+						name: i,
+					}
+				}
 				e := simple.Edge{
 					T: dict[i],
 					F: n,
 				}
 				g.SetEdge(e)
 			}
-		case n.ValueInfoProto != nil:
 		default:
 		}
 	}
@@ -88,6 +111,7 @@ func main() {
 
 type node struct {
 	id             int64
+	name           string
 	NodeProto      *onnx.NodeProto
 	ValueInfoProto *onnx.ValueInfoProto
 }
@@ -96,12 +120,13 @@ func (n node) ID() int64 {
 	return n.id
 }
 
-func (n node) GetName() string {
+func (n node) DOTID() string {
 	switch {
 	case n.NodeProto != nil:
-		return *n.NodeProto.Name
+		return fmt.Sprintf(`"%v_%v"`, n.ID(), *n.NodeProto.Name)
 	case n.ValueInfoProto != nil:
-		return *n.ValueInfoProto.Name
+		return fmt.Sprintf(`"%v_%v"`, n.ID(), *n.ValueInfoProto.Name)
+	default:
+		return fmt.Sprintf(`"%v_%v"`, strconv.Itoa(int(n.ID())), n.name)
 	}
-	return ""
 }
