35ff40c1125c4c7fba842051bee4e2d911f1c363
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-08-16 22:06:44 +0200

    Variable names starts to be normalized in the code

--- initializer.go
+++ initializer.go
@@ -10,38 +10,38 @@ import (
 
 // newValue returns a Gorgonia compatible value from a onnx.ValueInfoProto structure
 // By now, it will return a tensor.Tensor
-func newTensor(t *onnx.TensorProto) (tensor.Tensor, error) {
+func newTensor(tx *onnx.TensorProto) (tensor.Tensor, error) {
 	// Get the data type
-	dt, err := toDtype(t.DataType)
+	dt, err := toDtype(tx.DataType)
 	if err != nil {
 		return nil, err
 	}
-	var size = make([]int, len(t.Dims))
-	for i := range t.Dims {
-		size[i] = int(t.Dims[i])
+	var size = make([]int, len(tx.Dims))
+	for i := range tx.Dims {
+		size[i] = int(tx.Dims[i])
 	}
 	var backing interface{}
 	switch dt {
 	case tensor.Float32:
-		if len(t.FloatData) == 0 {
+		if len(tx.FloatData) == 0 {
 			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
 		}
-		backing = t.FloatData
+		backing = tx.FloatData
 	case tensor.Float64:
-		if len(t.DoubleData) == 0 {
+		if len(tx.DoubleData) == 0 {
 			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
 		}
-		backing = t.DoubleData
+		backing = tx.DoubleData
 	case tensor.Int64:
-		if len(t.Int64Data) == 0 {
+		if len(tx.Int64Data) == 0 {
 			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
 		}
-		backing = t.Int64Data
+		backing = tx.Int64Data
 	case tensor.Int32:
-		if len(t.Int32Data) == 0 {
+		if len(tx.Int32Data) == 0 {
 			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
 		}
-		backing = t.Int32Data
+		backing = tx.Int32Data
 	default:
 		return nil, fmt.Errorf("Backend not yet implemented")
 
@@ -51,13 +51,13 @@ func newTensor(t *onnx.TensorProto) (tensor.Tensor, error) {
 }
 
 // add the value v to the graph g and return the added node
-func (d *Decoder) addTensor(v *onnx.TensorProto) (*gorgonia.Node, error) {
-	val, err := newTensor(v)
+func (d *Decoder) addTensor(tx *onnx.TensorProto) (*gorgonia.Node, error) {
+	t, err := newTensor(tx)
 	if err != nil {
 		return nil, err
 	}
-	n := gorgonia.NodeFromAny(d.g, val, gorgonia.WithName(*v.Name))
+	n := gorgonia.NodeFromAny(d.g, t, gorgonia.WithName(*tx.Name))
 	// TODO: check if NAme is empty (according to the SPEC it must be filled but anyway)
-	d.db[v.GetName()] = n
+	d.db[tx.GetName()] = n
 	return n, nil
 }
