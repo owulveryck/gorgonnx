f3a84e4b18cdd769883b8cc46e14bd1e27781b42
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-08-23 22:36:27 +0200

    Treating the constants as they need to ne

--- initializer.go
+++ initializer.go
@@ -6,12 +6,19 @@ import (
 )
 
 // add the value v to the graph g and return the added node
-func (d *Decoder) addTensor(tx *onnx.TensorProto) (*gorgonia.Node, error) {
+func (d *Decoder) addTensor(tx *onnx.TensorProto, constant bool) (*gorgonia.Node, error) {
 	t, err := NewTensor(tx)
 	if err != nil {
 		return nil, err
 	}
-	n := gorgonia.NodeFromAny(d.g, t, gorgonia.WithName(*tx.Name))
+	var n *gorgonia.Node
+	if constant {
+		n = gorgonia.NewConstant(t, gorgonia.WithName(*tx.Name))
+		n = d.g.AddNode(n)
+	} else {
+
+		n = gorgonia.NodeFromAny(d.g, t, gorgonia.WithName(*tx.Name))
+	}
 	// TODO: check if NAme is empty (according to the SPEC it must be filled but anyway)
 	d.db[tx.GetName()] = n
 	return n, nil
