3bf7aa2758905d61cbc9cb06423de0d301a1cdd0
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-08-23 16:17:44 +0200

    Utilities has been moved to mnist as it is specific

--- mnist/utilities/decode_input.go
+++ mnist/utilities/decode_input.go
@@ -14,18 +14,19 @@ import (
 
 // This little utility reads an encoded tensor and draw a picture
 func main() {
+	// Read the input
 	b, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	sampleTestData := new(onnx.TensorProto)
 	err = sampleTestData.Unmarshal(b)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	t, err := gorgonnx.Tensorize(sampleTestData)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	width := t.Shape()[2]
 	height := t.Shape()[3]
@@ -34,7 +35,7 @@ func main() {
 		for h := 0; h < height; h++ {
 			v, err := t.At(0, 0, w, h)
 			if err != nil {
-				log.Fatal(err)
+				panic(err)
 			}
 			im.Set(w, h, color.Gray{uint8(v.(float32))})
 		}
@@ -42,7 +43,23 @@ func main() {
 	enc := png.Encoder{}
 	err = enc.Encode(os.Stdout, im)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	// Read the output...
+	b, err = ioutil.ReadFile(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	sampleTestData = new(onnx.TensorProto)
+	err = sampleTestData.Unmarshal(b)
+	if err != nil {
+		panic(err)
+	}
+	t, err = gorgonnx.Tensorize(sampleTestData)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Expected outout:", t)
 
 }
