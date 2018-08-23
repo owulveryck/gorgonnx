cec50e6a316bf65845b5867dda5b043bb12f4294
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-08-23 17:39:56 +0200

    Merge of two similar functions

--- initializer.go
+++ initializer.go
@@ -1,58 +1,13 @@
 package gorgonnx
 
 import (
-	"fmt"
-
 	"github.com/owulveryck/gorgonnx/onnx"
 	"gorgonia.org/gorgonia"
-	"gorgonia.org/tensor"
 )
-
-// newValue returns a Gorgonia compatible value from a onnx.ValueInfoProto structure
-// By now, it will return a tensor.Tensor
-func newTensor(tx *onnx.TensorProto) (tensor.Tensor, error) {
-	// Get the data type
-	dt, err := toDtype(tx.DataType)
-	if err != nil {
-		return nil, err
-	}
-	var size = make([]int, len(tx.Dims))
-	for i := range tx.Dims {
-		size[i] = int(tx.Dims[i])
-	}
-	var backing interface{}
-	switch dt {
-	case tensor.Float32:
-		if len(tx.FloatData) == 0 {
-			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
-		}
-		backing = tx.FloatData
-	case tensor.Float64:
-		if len(tx.DoubleData) == 0 {
-			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
-		}
-		backing = tx.DoubleData
-	case tensor.Int64:
-		if len(tx.Int64Data) == 0 {
-			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
-		}
-		backing = tx.Int64Data
-	case tensor.Int32:
-		if len(tx.Int32Data) == 0 {
-			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
-		}
-		backing = tx.Int32Data
-	default:
-		return nil, fmt.Errorf("Backend not yet implemented")
-
-	}
-
-	return tensor.New(tensor.WithShape(size...), tensor.WithBacking(backing), tensor.Of(dt)), nil
-}
 
 // add the value v to the graph g and return the added node
 func (d *Decoder) addTensor(tx *onnx.TensorProto) (*gorgonia.Node, error) {
-	t, err := newTensor(tx)
+	t, err := NewTensor(tx)
 	if err != nil {
 		return nil, err
 	}
--- tensor.go
+++ tensor.go
@@ -3,7 +3,7 @@ package gorgonnx
 import (
 	"bytes"
 	"encoding/binary"
-	"errors"
+	"fmt"
 	"io"
 	"math"
 
@@ -11,22 +11,25 @@ import (
 	"gorgonia.org/tensor"
 )
 
-// Tensorize a TensorProto into a tensor.Tensor
-func Tensorize(tx *onnx.TensorProto) (tensor.Tensor, error) {
-	var t tensor.Tensor
-	// Get the datatype
+// NewTensor returns a Gorgonia compatible tensor from a onnx.TensorProto structure
+func NewTensor(tx *onnx.TensorProto) (tensor.Tensor, error) {
+	// Get the data type
 	dt, err := toDtype(tx.DataType)
 	if err != nil {
 		return nil, err
 	}
-	switch {
-	case tx.RawData != nil:
-		switch dt {
-		case tensor.Float32:
-			var backing []float32
+	var size = make([]int, len(tx.Dims))
+	for i := range tx.Dims {
+		size[i] = int(tx.Dims[i])
+	}
+	opts := []tensor.ConsOpt{tensor.WithShape(size...), tensor.Of(dt)}
+	switch dt {
+	case tensor.Float32:
+		if len(tx.FloatData) == 0 {
 			buf := bytes.NewReader(tx.RawData)
 			element := make([]byte, 4)
 			var err error
+			var backing []float32
 			for {
 				var n int
 				n, err = buf.Read(element)
@@ -39,12 +42,28 @@ func Tensorize(tx *onnx.TensorProto) (tensor.Tensor, error) {
 			if err != io.EOF {
 				return nil, err
 			}
-			t = tensor.New(tensor.WithShape(toIntSlice(tx.Dims)...), tensor.WithBacking(backing))
-		default:
-			return nil, errors.New("type not yet implemented for rawdata")
+			opts = append(opts, tensor.WithBacking(backing))
+		} else {
+			opts = append(opts, tensor.WithBacking(tx.FloatData))
+		}
+	case tensor.Float64:
+		if len(tx.DoubleData) == 0 {
+			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
+		}
+		opts = append(opts, tensor.WithBacking(tx.DoubleData))
+	case tensor.Int64:
+		if len(tx.Int64Data) == 0 {
+			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
+		}
+		opts = append(opts, tensor.WithBacking(tx.Int64Data))
+	case tensor.Int32:
+		if len(tx.Int32Data) == 0 {
+			return nil, fmt.Errorf("No data found. Maybe a raw data but not yet implemented")
 		}
 	default:
-		return nil, errors.New("No data found or not implemented")
+		return nil, fmt.Errorf("Backend not yet implemented")
+
 	}
-	return t, nil
+
+	return tensor.New(opts...), nil
 }
