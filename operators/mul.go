e8667da769256c05f99380a826f430a00c63b86f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-10-17 14:17:47 +0200

    HadamardMul ok

---
+++ operators/mul.go
@@ -0,0 +1,38 @@
+package operators
+
+import (
+	onnx "github.com/owulveryck/onnx-go"
+	"gorgonia.org/gorgonia"
+)
+
+// Mul operator
+// https://github.com/onnx/onnx/blob/master/docs/Operators.md#Mul
+type Mul struct {
+	name string
+}
+
+// Init ...
+func (o *Mul) Init(attrs []*onnx.AttributeProto) error {
+	o.name = "Mul"
+	return nil
+}
+
+// Apply ...
+func (o *Mul) Apply(input ...*gorgonia.Node) ([]*gorgonia.Node, error) {
+	if len(input) != 2 {
+		return nil, &ErrBadArity{
+			Operator:      o.name,
+			ExpectedInput: 2,
+			ActualInput:   len(input),
+		}
+	}
+	if len(input[0].Shape()) != len(input[1].Shape()) {
+		return nil, &onnx.ErrNotImplemented{
+			Operator: o.name,
+			Message:  "TODO: implement the broadcast",
+		}
+	}
+	n, err := gorgonia.HadamardProd(input[0], input[1])
+	return []*gorgonia.Node{n}, err
+
+}
