1238aeff258d2d0bac56c359483e5999f51a9d76
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-10-17 16:00:49 +0200

    Relu ok

--- operators/relu.go
+++ operators/relu.go
@@ -3,6 +3,7 @@ package operators
 import (
 	onnx "github.com/owulveryck/onnx-go"
 	"gorgonia.org/gorgonia"
+	nnops "gorgonia.org/gorgonia/ops/nn"
 )
 
 // Relu operator
@@ -14,26 +15,19 @@ type Relu struct {
 // Init ...
 func (o *Relu) Init(attrs []*onnx.AttributeProto) error {
 	o.name = "Relu"
-	return &onnx.ErrNotImplemented{
-		Operator: o.name,
-		Message:  "Not implemented yet",
-	}
+	return nil
 }
 
 // Apply ...
 func (o *Relu) Apply(input ...*gorgonia.Node) ([]*gorgonia.Node, error) {
-	/*
-		if len(input) != 2 {
-			return nil, &ErrBadArity{
-				Operator:      o.name,
-				ExpectedInput: 2,
-				ActualInput:   len(input),
-			}
+	if len(input) != 1 {
+		return nil, &ErrBadArity{
+			Operator:      o.name,
+			ExpectedInput: 1,
+			ActualInput:   len(input),
 		}
-	*/
-	return nil, &onnx.ErrNotImplemented{
-		Operator: o.name,
-		Message:  "Not implemented yet",
 	}
+	n, err := nnops.Rectify(input[0])
 
+	return []*gorgonia.Node{n}, err
 }
