2b34a0f23c41ec293ec1b9afb814f04e44aeaa17
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-11-05 21:29:40 +0100

    WIP

--- operators/unsqueeze_wip.go
+++ operators/unsqueeze_wip.go
@@ -9,22 +9,30 @@ import (
 // https://github.com/onnx/onnx/blob/master/docs/Operators.md#Unsqueeze
 type Unsqueeze struct {
 	name string
+	Axes []int64
 }
 
 // Init ...
 func (o *Unsqueeze) Init(attrs []*onnx.AttributeProto) error {
 	o.name = "Unsqueeze"
-	return &onnx.ErrNotImplemented{
-		Operator: o.name,
-		Message:  "Not implemented yet",
+	type attributes struct {
+		Axes []int64 `attributeName:"axes" required:"true"`
 	}
+	var attr attributes
+	err := onnx.UnmarshalAttributes(attrs, &attr)
+	if err != nil {
+		return err
+	}
+	o.Axes = attr.Axes
+
+	return nil
 }
 
 // Apply ...
 func (o *Unsqueeze) Apply(input ...*gorgonia.Node) ([]*gorgonia.Node, error) {
 	/*
 		if len(input) != 2 {
-			return nil, &ErrBadArity{
+			return nil, &ErrlBadArity{
 				Operator:      o.name,
 				ExpectedInput: 2,
 				ActualInput:   len(input),
